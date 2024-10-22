c33bdc11459bb9dbb311d803e31aa321f58c39f5
Alexander Sirotin <a@AA.local>
2024-10-22 23:30:34 +0300

    git blame, git diff

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,29 +1,30 @@
 return {
-	{
-		"echasnovski/mini.diff",
-		version = "*",
-		config = function()
-			require("mini.diff").setup()
-		end,
-	},
-	{
-		"FabijanZulj/blame.nvim",
-		config = function()
-			require("blame").setup({
-				colors = {
-					"#ffffff",
-					"#897973",
-					"#aaaaaa",
-					"#848484",
-					"#B2BDC4",
-					"#e3d6be",
-					"#ebdbb2",
-					"#b8bb26",
-					"#fabd2f",
-					"#83a598",
-				},
-			})
-			vim.keymap.set("n", "<leader>gt", "<cmd>BlameToggle<CR>", { desc = "blame" })
-		end,
-	},
+  {
+    "echasnovski/mini.diff",
+    version = "*",
+    config = function()
+      require("mini.diff").setup()
+      vim.keymap.set("n", "<leader>gd", "<cmd>lua MiniDiff.toggle_overlay()<CR>", { desc = "toggle_overlay" })
+    end,
+  },
+  {
+    "FabijanZulj/blame.nvim",
+    config = function()
+      require("blame").setup({
+        colors = {
+          "#ffffff",
+          "#897973",
+          "#aaaaaa",
+          "#848484",
+          "#B2BDC4",
+          "#e3d6be",
+          "#ebdbb2",
+          "#b8bb26",
+          "#fabd2f",
+          "#83a598",
+        },
+      })
+      vim.keymap.set("n", "<leader>gt", "<cmd>BlameToggle<CR>", { desc = "blame" })
+    end,
+  },
 }
