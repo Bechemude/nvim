df653239ca35fbfe1f57afb8991e9768c9d97c36
Alexander Sirotin <as@MacBookPro.netis.cc>
2025-06-02 12:41:48 +0300

    update to debugger

---
+++ lua/plugins/debugprint.lua
@@ -0,0 +1,41 @@
+return {
+  "andrewferrier/debugprint.nvim",
+
+  opts = {
+    keymaps = {
+      normal = {
+        plain_below = "g?p",
+        plain_above = "g?P",
+        variable_below = "g?v",
+        variable_above = "g?V",
+        variable_below_alwaysprompt = "",
+        variable_above_alwaysprompt = "",
+        surround_plain = "g?sp",
+        surround_variable = "g?sv",
+        surround_variable_alwaysprompt = "",
+        textobj_below = "g?o",
+        textobj_above = "g?O",
+        textobj_surround = "g?so",
+        toggle_comment_debug_prints = "",
+        delete_debug_prints = "",
+      },
+      insert = {
+        plain = "<C-G>p",
+        variable = "<C-G>v",
+      },
+      visual = {
+        variable_below = "g?v",
+        variable_above = "g?V",
+      },
+    },
+    -- … Other options
+  },
+
+  dependencies = {
+    "echasnovski/mini.hipatterns",       -- Optional: Needed for line highlighting ('fine-grained' hipatterns plugin)
+    "nvim-telescope/telescope.nvim",     -- Optional: If you want to use the `:Debugprint search` command with telescope.nvim
+  },
+
+  lazy = false,    -- Required to make line highlighting work before debugprint is first used
+  version = "*",   -- Remove if you DON'T want to use the stable version
+}
