0c0b8fa013177a73c559e588d68be7ec446b18ad
a.sirotin <asirotin@stepstg.com>
2026-07-28 14:35:34 +0300

    change theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,7 +1,13 @@
 return {
-	"zenbones-theme/zenbones.nvim",
-	dependencies = { "rktjmp/lush.nvim" },
+	"neanias/everforest-nvim",
+	version = false,
+	lazy = false,
+	priority = 1000, -- make sure to load this before all the other start plugins
+	-- Optional; default configuration will be used if setup isn't called.
 	config = function()
-		vim.cmd.colorscheme("zenbones")
+		vim.cmd.colorscheme("everforest")
+		require("everforest").setup({
+			-- Your config here
+		})
 	end,
 }
