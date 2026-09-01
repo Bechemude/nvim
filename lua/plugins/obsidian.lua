116df4a07a72a802e49ca3dee517fedf7e0fef1d
a.sirotin <bechemude@gmail.com>
2026-09-01 19:29:24 +0300

    daily notes

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -31,9 +31,9 @@ return {
 
 		daily_notes = {
 			-- Optional, if you keep daily notes in a separate directory.
-			folder = "notes/dailies",
+			folder = "notes/daily",
 			-- Optional, if you want to change the date format for the ID of daily notes.
-			date_format = "%Y-%m-%d",
+			date_format = "%Y-%m-%d/%Y-%m-%d",
 			-- Optional, if you want to change the date format of the default alias of daily notes.
 			alias_format = "%B %-d, %Y",
 			-- Optional, default tags to add to each new daily note created.
